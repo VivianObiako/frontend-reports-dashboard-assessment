2aa84997da5fbf9c2dc4a04f93fb12ae39b1bbc1
Oluchukwu Obiako <Oluchukwu.Obiako@fcmb.com>
2024-08-21 02:13:12 +0100

    style: added colors and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {100: "#667185", 200: "#687385", 300:"#697386", 400: "#4F566B"},
+        blue: {100: "#5469D4", },
+        purple: {100: "#625AFA"},
+        gray: {100: "#E3E8EE"},
+        black: {100: "#171717", 200: "#1A1F36"},
+        white: {100: "#ffffff"},
+        green: {100: "#0E6245", 200: "#CBF4C9"},
+        orange: {100: "#9C3F0F", 200: "#F8E5BA"}
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
